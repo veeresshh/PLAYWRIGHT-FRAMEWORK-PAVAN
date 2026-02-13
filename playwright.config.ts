ce1c5a9e81c9c43c2a0e55a86a0d123eabaf73c3
veeresshh <veeresh0270@gmail.com>
2026-02-13 12:34:23 +0530

    fd

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   workers: 1,
 
   reporter: [
-    ['html', {title: 'Playwright Test Report'}],
+    ['html', {title: 'Playwright Test Execution Report'}],
     ['allure-playwright'],
     ['list']
   ],
